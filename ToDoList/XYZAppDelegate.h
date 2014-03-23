a7904f9422c723173d83c162038df57450bc0838
Abdalla Mohamed <Abdalla@Abdallas-MacBook-Pro.local>
2014-03-22 19:02:30 -0700

    Initial Commit

---
+++ ToDoList/XYZAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  XYZAppDelegate.h
+//  ToDoList
+//
+//  Created by Abdalla Mohamed on 3/22/14.
+//  Copyright (c) 2014 ABDALLAMOHAMED. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ ToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by Abdalla Mohamed on 3/22/14.
+//  Copyright (c) 2014 ABDALLAMOHAMED. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XYZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XYZAppDelegate class]));
+    }
+}
